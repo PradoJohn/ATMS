53404ca8ee5d8a5e55372e372e48c839abe9f31c
John Prado <pradobanz@gmail.com>
2023-04-14 22:02:50 +0800

    Refined Templates

--- atmsusers/views.py
+++ atmsusers/views.py
@@ -9,7 +9,7 @@ def login_user(request):
         user = authenticate(request, username=username, password=password)
         if user is not None:
             login(request, user)
-            return redirect ('dashboard')
+            return redirect ('home')
         else:
             messages.success(request, ('Invalid Username or Password. Try Again.'))
             return redirect ('login')
