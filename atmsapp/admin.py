40e79e4d1227210c30a674b5d18f72017748e9aa
Prado <pradobanz@gmail.com>
2023-04-20 11:48:07 +0800

    Established DB

--- atmsapp/admin.py
+++ atmsapp/admin.py
@@ -4,3 +4,9 @@ from django.contrib import admin
 from .models import *
 
 
+admin.site.register(GPSDevice)
+admin.site.register(GPSCoordinate)
+admin.site.register(Vehicle)
+admin.site.register(GPSOperator)
+admin.site.register(PollutionSensor)
+admin.site.register(PollutionData)
